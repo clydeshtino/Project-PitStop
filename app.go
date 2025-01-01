0353effccef899cdea8f18d84a96595a8d1ca540
clydeshtino <clydeshtino@gmail.com>
2025-01-01 16:07:52 -0500

    More file structure setup, intialized tailwindcss.

---
+++ app.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	router "Project-Pitstop/src-app/routes"
+	session "Project-Pitstop/src-app/shared/session"
+	"Project-Pitstop/src-app/shared/view"
+)
+
+func main() {
+	log.Print("\nStarting Project-Pitstop.....")
+	log.Print("\nProject-Pitstop is now running.")
+	view.ParseTemplates(templates)
+}
