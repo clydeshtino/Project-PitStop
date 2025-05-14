c892393c5758df54e00d625a993ec112025e60f7
clydeshtino <clydeshtino@gmail.com>
2025-05-14 12:22:57 -0400

    Set up basic routing structure, create go.mod, FAQs page scrapped

---
+++ go.mod
@@ -0,0 +1,6 @@
+module Project-Pitstop
+go 1.23.4
+
+require (
+    github.com/gorilla/mux v1.2.3
+)
--- src-app/routes/routes.go
+++ src-app/routes/routes.go
@@ -1,12 +1,32 @@
 package routes
 
-/* import ("embed"
-"net/http"
-"path/filepath"
-"runtime"
-"Project-Pitstop/src-app/controllers"
+import (
+	"net/http"
+)
 
-"github.com/gorilla/mux"
-)*/
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// TODO: Define function to show main page, call it here
+	} else if r.Method == "POST" {
+		// TODO: Define process function for main, call it here
+	}
+}
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// TODO: Define function in controller to show news, call here
+	} else if r.Method == "POST" {
+		// TODO: Define function in controller to process news, then call here
+	}
+}
 
-// TODO: Implement routes to each page
+func compareHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// TODO: Define controller function showing compare then call
+	} else if r.Method == "POST" {
+		// TODO: Define process controller function, then call it here.
+	}
+}
+
+/*func routes() *mux.router {
+
+}*/
